8385d2d12cade93235b4bcde79334763f2b28f2e
Dr. Brian Bansenauer <brianb@cascadia.edu>
2023-01-05 12:10:02 -0800

    Initial commit

---
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuckySpin.Controllers
+{
+    public class SpinnerController : Controller
+    {
+
+        public IActionResult Index(int luck) 
+        {
+
+
+            return new ContentResult { Content = "<h1>We're Ready to Spin with Controllers</h1>", ContentType="text/html"};
+        }
+    }
+}
---
+++ LuckySpin/Program.cs
@@ -0,0 +1,38 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+/* Install Services using the builder.Services methods
+ *  TODO: use the AddControllers method to enable controllers
+ */  
+
+//Builds the app with the added services
+var app = builder.Build();
+
+
+/* Configure Middleware in the HTTP Request Pipeline
+   • "UseStaticFiles" to recognizste static folders and files in the wwwroot directory
+   • TODO: add "UseRouting" to recognize custom "Routes" in place of folders and files
+   • "UseExceptionHandler" to provide a default error page when not in development
+ */
+app.UseStaticFiles();
+
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Spinner/Error");
+}
+
+
+/* Configure Routing with a general pattern
+ *  and a default setting if the URL is just the base site
+ *  TODO: Add a range(1,9) method to constrain luck between 1 and 9
+ */
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action}/{luck}",
+    defaults: new
+    {
+        controller = "Spinner",
+        action = "Index",
+        luck = 7
+    });
+
+app.Run();
+
