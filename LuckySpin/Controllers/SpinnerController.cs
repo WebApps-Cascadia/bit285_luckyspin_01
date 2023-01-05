7449bb89061c3bb227b2784091cfcb812c436ca6
Noah Ku <thenoahku@gmail.com>
2023-01-05 14:56:46 -0800

    finished

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -9,11 +9,44 @@ namespace LuckySpin.Controllers
     public class SpinnerController : Controller
     {
 
-        public IActionResult Index(int luck) 
+        public IActionResult Index(int luck)
         {
+            
+                System.Random random = new System.Random();
 
+                int[] spin = new int[] { random.Next(10), random.Next(10), random.Next(10) };
 
-            return new ContentResult { Content = "<h1>We're Ready to Spin with Controllers</h1>", ContentType="text/html"};
+                string[] nums = new string[]
+                {
+                "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"
+                };
+
+                string number = "";
+                for(int i = 0; i < luck; i++ )
+                {
+                    number = nums[i];
+                }
+
+                System.Text.StringBuilder htmlToShow =
+                  new System.Text.StringBuilder("<body><h1>Lucky " + number + "</h1><button onclick='history.go(0)'>Spin</button>");
+                htmlToShow.Append("<div>" + spin[0] + "</div>");
+                htmlToShow.Append("<div>" + spin[1] + "</div>");
+                htmlToShow.Append("<div>" + spin[2] + "</div>");
+
+                for (int i = 0; i < 3; i++)
+                {
+
+                    if (spin[i] == luck)
+                    {
+                        htmlToShow.Append("<img src='http://faculty.cascadia.edu/brianb/images/LuckySevenExercise.jpg'/></body>");
+                        break;
+                    }
+                }
+
+                
+            
+
+            return new ContentResult { Content = htmlToShow.ToString(), ContentType="text/html"};
         }
     }
 }
